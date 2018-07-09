831918cb465de7a89c4495a04143c01f9d022df9
David Zetterdahl <david@viscando.com>
2018-07-09 17:50:08 +0200

    Added the rope bat!

---
+++ Assets/Code/Traps/Rope/RopeStart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Code.Traps.Rope
+{
+    /// <summary>
+    /// The point from which the rope starts.
+    /// </summary>
+    public class RopeStart : MonoBehaviour
+    {
+        void Start()
+        {
+            var spriteRend = GetComponent<SpriteRenderer>();
+            if (spriteRend != null)
+            {
+                spriteRend.enabled = false;
+            }
+        }
+    }
+}
