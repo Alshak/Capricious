fdb88d3334d8219557760d1417e781823d4adab5
David Zetterdahl <david@viscando.com>
2018-07-10 09:56:29 +0200

    Last traps added.

---
+++ Assets/Code/Gibs/BloodSpawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Assets.Code.Gibs
+{
+    public class BloodSpawner : MonoBehaviour
+    {
+        public int AmountOfBloodDrops = 10;
+
+        public GameObject BloodDrop;
+
+        void Start()
+        {
+            Spawn();
+        }
+
+        private void Spawn()
+        {
+            for (int i = 0; i < AmountOfBloodDrops; i++)
+            {
+                BloodDrop.transform.position = transform.position;
+                BloodDrop.transform.rotation = transform.rotation;
+                BloodDrop.transform.eulerAngles = new Vector3(0, 0, i + 10 * AmountOfBloodDrops);
+                Instantiate(BloodDrop);
+            }
+        }
+    }
+}
---
+++ Assets/Code/Spawning/PlayerRespawner.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Code.Spawning
+{
+    public class PlayerRespawner : MonoBehaviour
+    {
+        public Spawnpoint CurrentSpawn;
+
+        public void RespawnPlayer(GameObject player)
+        {
+            Debug.Log("RESPAWN!");
+            player.transform.position = CurrentSpawn.transform.position;
+            Rigidbody2D rigid = player.GetComponent<Rigidbody2D>();
+
+            if (rigid != null)
+            {
+                rigid.transform.position = CurrentSpawn.transform.position;
+                rigid.velocity = Vector3.zero;
+                rigid.angularVelocity = 0f;
+            }
+        }
+    }
+}
