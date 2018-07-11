655bce0009670f9653b51a9eacf954dc1102971e
Alshak <alain.shakour@gmail.com>
2018-07-11 21:33:09 +0200

    Lets Swing

--- Assets/Code/Traps/Swinging.cs
+++ Assets/Code/Traps/Swinging.cs
@@ -12,6 +12,9 @@ namespace Assets.Code.Traps
         public bool IsActivated = true;
         float timerSinceStart;
         public float Speed = 1f;
+        float minZ = 999;
+        float maxZ = -999;
+        bool clockwise = false;
         void Start()
         {
             startPos = transform.rotation;
@@ -20,11 +23,28 @@ namespace Assets.Code.Traps
 
         void Update()
         {
-            timerSinceStart += Time.deltaTime * Speed;
+            if(clockwise && transform.rotation.z > 0.99999f)
+            {
+                clockwise = false;
+            }else if(!clockwise && transform.rotation.z < 0)
+            {
+                clockwise = true;
+            }
+
             if (Time.timeScale > 0)
             {
-                transform.Rotate(new Vector3(0, 0, 1.5f * Speed * Mathf.Sin(timerSinceStart)));
+                if (!clockwise)
+                {
+                    transform.Rotate(new Vector3(0, 0, Speed));
+                }
+                else
+                {
+                    transform.Rotate(new Vector3(0, 0, -Speed));
+                }
             }
+            minZ = Mathf.Min(minZ, transform.rotation.z);
+            maxZ = Mathf.Max(maxZ, transform.rotation.z);
+            Debug.Log(transform.rotation.z);
         }
     }
 }
