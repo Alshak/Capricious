dd5b46e688bf2934e895849c591f813fe2d50346
David Zetterdahl <david@viscando.com>
2018-07-12 21:37:34 +0200

    Main menu comic!

---
+++ Assets/Code/Intro/Pageflipper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Code.Intro
+{
+    public class Pageflipper : MonoBehaviour
+    {
+        private AudioSource pageSound;
+    }
+}
