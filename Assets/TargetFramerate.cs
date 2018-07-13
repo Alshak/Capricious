01521c3437ef4170fb2357c56a2fab93c6e8886d
Alshak <alain.shakour@gmail.com>
2018-07-13 10:33:28 +0200

    Improve everything

---
+++ Assets/TargetFramerate.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetFramerate : MonoBehaviour {
+    void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
+}
