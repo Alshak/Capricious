ddf48f148ca103d38ad682a16abbe543877533e5
David Zetterdahl <david@viscando.com>
2018-07-13 18:47:22 +0200

    Pause menu and credits created.

---
+++ Assets/Code/UI/PauseMenu.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace Assets.Code.UI
+{
+    public class PauseMenu : MonoBehaviour
+    {
+        private bool isVisible = false;
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+            if (CrossPlatformInputManager.GetButtonDown("PauseButton"))
+            {
+                if (Time.timeScale == 0)
+                {
+                    Time.timeScale = 1;
+                    ShowMenu();
+
+                }
+                else
+                {
+                    Time.timeScale = 0;
+                    HideMenu();
+                }
+            }
+        }
+
+        private void ShowMenu()
+        {
+
+        }
+
+        private void HideMenu()
+        {
+
+        }
+    }
+}
