c9e058f596356c42dc9c026a6a60b28880905a63
Alshak <alain.shakour@gmail.com>
2018-07-13 14:34:47 +0200

    fix things

--- Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -382,9 +382,11 @@ namespace UnityStandardAssets._2D
             {
                 m_Rigidbody2D.velocity = new Vector2(m_Rigidbody2D.velocity.x, m_Rigidbody2D.velocity.y * 0.75f);
             }
-            m_Rigidbody2D.velocity = new Vector2(Mathf.Clamp(m_Rigidbody2D.velocity.x, -20, 20), Mathf.Clamp(m_Rigidbody2D.velocity.y, -20, 20));
+            maximumVelocity = Mathf.Max(maximumVelocity, m_Rigidbody2D.velocity.y);
+            Debug.Log(maximumVelocity);
+            m_Rigidbody2D.velocity = new Vector2(Mathf.Clamp(m_Rigidbody2D.velocity.x, -20, 20), Mathf.Clamp(m_Rigidbody2D.velocity.y, -20, 14));
         }
-
+        float maximumVelocity = -999;
 
         private void UpdateParticles(Vector3 speed)
         {
