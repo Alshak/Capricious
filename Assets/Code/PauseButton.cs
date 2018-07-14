bd0753194d2893e6bf4618e203d424a7d65e0d60
Alshak <alain.shakour@gmail.com>
2018-07-14 10:20:03 +0200

    Pause screen

--- Assets/Code/PauseButton.cs
+++ Assets/Code/PauseButton.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class PauseButton : MonoBehaviour {
+    public GameObject pausePanel;
 	void Update () {
         if (CrossPlatformInputManager.GetButtonDown("PauseButton"))
         {
             if (Time.timeScale == 0)
             {
                 Time.timeScale = 1;
+                pausePanel.SetActive(false);
             }
             else
             {
                 Time.timeScale = 0;
+                pausePanel.SetActive(true);
             }
         }
 
